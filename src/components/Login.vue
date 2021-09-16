1dd5350a2fd5969f7819a77fca68fea248072e19
Deemo <408457807@qq.com>
2021-09-16 21:30:20 +0800

    完成了登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avator_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form
+        :model="loginForm"
+        :rules="rules"
+        ref="loginFormRef"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" :plain="true" @click="submitForm">登录</el-button>
+          <el-button type="info" @click="resetForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <vue-particles
+      color="#dedede"
+      :particleOpacity="0.8"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.6"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    >
+    </vue-particles>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入活用户名称', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入活用户密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (valid) {
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) {
+            return this.$message.error('登录失败')
+          }
+          this.$message.success('登录成功')
+          // 登录成功保存 token
+          window.sessionStorage.setItem('token', res.data.token)
+          this.$router.push('/home')
+        } else {
+          this.$message.warning('提交失败，请检查用户信息')
+          return false
+        }
+      })
+    },
+    // 重置表单
+    resetForm() {
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scope>
+.login_container {
+  // background: url(./img/sourc  e.gif);
+  background-color: #252854;
+  height: 100%;
+  overflow: hidden;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  .avator_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
