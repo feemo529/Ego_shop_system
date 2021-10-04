056e6746ad7cffbb5e3305dad500bec2e1eddff7
Deemo <408457807@qq.com>
2021-10-04 22:14:32 +0800

    开始商品分类开发

---
+++ src/components/goods/Goods.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
